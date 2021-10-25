9dc96a01b538278cf60cd69d78d063ca513b1f63
Ravi Sharma <ravisharma45935@gmail.com>
2021-10-25 19:58:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let screen = document.getElementById('screen');
+let button = document.querySelectorAll('button');
+let screenValue = '';
+
+for(item of button){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        
+    if(buttonText == 'X'){
+        buttonText = '*';
+        screenValue += buttonText;
+        screen.value = screenValue;
+    }
+
+    else if(buttonText == 'C'){
+        screenValue = '';
+        screen.value = screenValue;
+    }
+    else if (buttonText == '='){
+        screen.value = eval(screenValue);
+    }
+
+    else{
+        screenValue += buttonText;
+        screen.value = screenValue;
+    }
+    })
+}
